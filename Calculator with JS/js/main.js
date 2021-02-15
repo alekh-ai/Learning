b7b706f668609dfae5df15665e0e475973961fc2
alekh-ai <78842369+alekh-ai@users.noreply.github.com>
2021-02-15 14:45:31 +0530

    Minor Update

--- Calculator with JS/js/main.js
+++ Calculator with JS/js/main.js
@@ -1,72 +1,140 @@
-
 // Action - Buttons
-const output = document.querySelector("#output")
-const _clear = document.querySelector("#action-clear")
-const _add = document.querySelector("#action-add")
-const _result = document.querySelector("#action-result")
+const _clear = document.querySelector("#clear");
+const _delete = document.querySelector("#delete");
+const _result = document.querySelector("#result");
 
+const currentState = document.querySelector(".current");
+const historyState = document.querySelector(".history");
+
+// Operation Buttons
+const _sub = document.querySelector("#sub");
+const _mul = document.querySelector("#mul");
+const _div = document.querySelector("#div");
+const _add = document.querySelector("#add");
 
 // Global Variables
 let total = 0;
-let current = '';
-let outputNums = ''
+let newState = "";
+let oldState = "";
+onScreen = "";
+lastOperation = "";
+toSub = [];
+
+const reset = () => {
+	total = 0;
+	newState = "";
+	oldState = "";
+	onScreen = "";
+	toSub = [];
+	lastOperation = "";
+};
+
+const renderOutput = (state) => {
+	currentState.innerText = state;
+};
+
+// Operations
+const onSubtraction = () => {
+	lastOperation = "sub";
+	if (toSub.length > 0) {
+		toSub[0] = toSub[0] - Number(newState);
+		newState = "";
+		renderOutput(toSub[0]);
+		return;
+	}
+	toSub.push(Number(newState));
+	newState = "";
+};
+
+const onMultiplication = () => {
+	lastOperation = "mul";
+	if (toSub.length > 0) {
+		toSub[0] = toSub[0] * Number(newState);
+		newState = "";
+		renderOutput(toSub[0]);
+		return;
+	}
+	toSub.push(Number(newState));
+	newState = "";
+};
+
+const onDivide = () => {
+	lastOperation = "div";
+	if (toSub.length > 0) {
+		toSub[0] = toSub[0] / Number(newState);
+		newState = "";
+		renderOutput(toSub[0]);
+		return;
+	}
+	toSub.push(Number(newState));
+	newState = "";
+};
+
+const onAddition = () => {
+	lastOperation = "add";
+	if (toSub.length > 0) {
+		toSub[0] = toSub[0] + Number(newState);
+		newState = "";
+		renderOutput(toSub[0]);
+		return;
+	}
+	toSub.push(Number(newState));
+	newState = "";
+};
 
 // Functions
-function onAddition(){
-    total += Number(current)
-    current = ''
-    outputNums += '+'
-    renderOutput()
-    console.log('Adding...')
+function onClear() {
+	reset();
+	renderOutput(total);
+	console.log("Erased Done!");
 }
 
-function onClear(){
-    total = 0
-    outputNums = ''
-    current = ''
-    renderOutput()
-    console.log('Erased Done!')
+function onResult() {
+	if (lastOperation == "sub") {
+		let result = toSub[0] - Number(newState);
+		renderOutput(result);
+		reset();
+	} else if (lastOperation == "mul") {
+		let result = toSub[0] * Number(newState);
+		renderOutput(result);
+		reset();
+	} else if (lastOperation == "div") {
+		let result = toSub[0] / Number(newState);
+		renderOutput(result);
+		reset();
+	} else if (lastOperation == "add") {
+		let result = toSub[0] + Number(newState);
+		renderOutput(result);
+		reset();
+	} else {
+		renderOutput(total);
+		reset();
+	}
 }
 
-function onResult(){
-    document.getElementById('output').innerText = total + Number(current)
-    console.log('Addition Done!')
-    total = 0
-    outputNums = ''
-    current = ''
+function sendNum(n) {
+	newState += n;
+	renderOutput(newState);
 }
 
-function sendNum(n){
-    outputNums += n
-    current += n
-    // document.getElementById('output').innerText = outputNums
-    console.log(`${n} is pressed`)
-    renderOutput()
-    
-}
-
-function pushAdd(n){
-    add.push(n)
-}
-
-function renderOutput(){
-    // console.log(`${total} : ${current}`)
-    if(15 > outputNums.length) {
-        document.getElementById('output').innerText = outputNums
-    }
-    return
-}
+function calculate() {}
 
 // Event Listeners
-_add.addEventListener('click', () => {
-    onAddition()
-})
-
-_clear.addEventListener('click', () => {
-    onClear()
-})
-
-_result.addEventListener('click', () => {
-    onResult()
-})
-
+_clear.addEventListener("click", () => {
+	onClear();
+});
+_result.addEventListener("click", () => {
+	onResult();
+});
+_sub.addEventListener("click", () => {
+	onSubtraction();
+});
+_mul.addEventListener("click", () => {
+	onMultiplication();
+});
+_add.addEventListener("click", () => {
+	onAddition();
+});
+_div.addEventListener("click", () => {
+	onDivide();
+});
